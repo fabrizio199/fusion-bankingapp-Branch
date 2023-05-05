58a5dae358ea2686fe08d24ad817f1f64506c0cd
fabrizio199 <78603022+fabrizio199@users.noreply.github.com>
2023-05-05 13:37:09 +0200

    Add files via upload

---
+++ Redis.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using Microsoft.AspNet.SignalR;
+
+namespace ConsoleApp7
+{
+    class Redis
+    {
+
+        public void Demo()
+        {
+            string redisConn1 = "Redis_1";
+
+            services.AddSignalR().AddStackExchangeRedis(null, redisConn1);
+
+
+            // Any connection or hub wire up and configuration should go here
+           // UseStackExchangeRedis("redis-server.cloudapp.net", 6379, "MyStrongPassword1234", redisConn2);
+        }
+    }
+        private string GetAuthorizationHeader()
+        {
+            return string.Empty;
+        }
+    }
+}
